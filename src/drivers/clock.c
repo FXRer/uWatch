d7a96b5c1e9a2b7eeea8ed311a1427e417e09797
Greg Davill <greg.davill@gmail.com>
2014-02-15 18:21:36 +1100

    added template for clock code.

---
+++ src/drivers/clock.c
@@ -0,0 +1,14 @@
+#include <msp430.h>
+#include "../cocoOS/cocoos.h"
+
+
+void clock_init(void)
+{
+	// start up a hardware clock here
+	
+
+}
+
+
+
+// interrupt here, call to os_tick(); to pass clock to cocoOS
